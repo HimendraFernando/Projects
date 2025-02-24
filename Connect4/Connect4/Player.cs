9fe87d9285d87424801a7d52d0c5ad9b8b59b66c
HimendraFernando <138412433+HimendraFernando@users.noreply.github.com>
2025-02-24 16:55:45 +1300

    Add files via upload

---
+++ Connect4/Connect4/Player.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Connect4
+{
+    internal class Player
+    {
+        private string _playerName;
+        private int _playerNumber;
+        private int _playerScore;
+        public Player()
+        {
+            _playerName = "User";
+            _playerNumber = 1;
+            _playerScore = 0;
+        }
+        public string PlayerName 
+        {
+            get { return _playerName; }
+            set { _playerName = value; } 
+        }
+        public int PlayerNumber
+        // Get and set the player number
+        {
+            get { return _playerNumber; }
+        }
+        public int PlayerScore
+        // Get and set the player score
+        {
+            get { return _playerScore; }
+            set { _playerScore = value; }
+        }
+
+        public void Winner()
+        // Display a message box when the player wins
+        {
+            MessageBox.Show(_playerName + " wins!", "Game Over", MessageBoxButtons.OK);
+        }
+    }
+}
